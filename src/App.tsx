1cba5049bba5067972be4cef68ca4298a4b5e7f2
Remi HOEPPE <remihoeppe@users.noreply.github.com>
2025-04-14 22:41:27 +0100

    Add CSV conversion helper methods to both string and object format

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react';
+import { convertCSVToObjects, convertCSVToText } from './data/Student';
 import './styles/App.scss';
 
 const App = () => {
-    return <div>App</div>;
+    const [data, setData] = useState<Record<string, string>[] | void>([]);
+
+    async function loadData() {
+        try {
+            // src/data/students-data.csv
+            const data = await convertCSVToObjects('./students-data.csv');
+            setData(data);
+        } catch (err) {
+            console.error('Error loading network data:', err);
+        }
+    }
+
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
+
+    useEffect(() => {
+        loadData();
+    }, []);
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    );
 };
 
 export default App;
